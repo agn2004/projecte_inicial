8c6bd4dd912665231f8dc46ec30fc07dd8f3f9c2
ALBERT-NOF\alber <agomilan@esliceu.net>
2021-11-09 11:41:42 +0100

    menu2

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+document.querySelector(".hamburguer").addEventListener("click", function(){
+	document.querySelector("#menu").classList.toggle("active");
+	document.querySelector("#menu").classList.toggle("hide");
+});
